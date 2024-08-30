6e461e7afbb2041dd8ee33c69e52f0e4c6d5d519
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2024-08-30 12:26:57 -0300

    wip

--- uploader/src/main/java/org/acme/GreetingResource.java
+++ uploader/src/main/java/org/acme/GreetingResource.java
@@ -13,4 +13,6 @@ public class GreetingResource {
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+    
 }
--- uploader/src/main/java/org/acme/MyRouteBuilder.java
+++ uploader/src/main/java/org/acme/MyRouteBuilder.java
@@ -10,30 +10,26 @@ public class MyRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-
         rest("/hello")
-            .get("/say")
+                .get("/say")
                 .to("direct:okResponse");
-                
+
         rest("/audios")
-            .post()
+                .post()
                 .consumes("application/octet-stream")
                 .to("direct:sendMessage");
-    
+
         from("direct:sendMessage")
-            .to("log:audiosEntrada?showBody=false&showHeaders=true")
-            .setHeader("TranscriptionID", () -> UUID.randomUUID().toString())
-            .to("amqp:queue:audios?exchangePattern=InOnly&useMessageIDAsCorrelationID=true")
-            .to("log:audiosSaida?showBody=false&showHeaders=true")
-            .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
-            .setBody(header("TranscriptionID"))
-        ;
+                .to("log:audiosEntrada?showBody=false&showHeaders=true")
+                .setHeader("TranscriptionID", () -> UUID.randomUUID().toString())
+                .to("activemq:queue:audios?exchangePattern=InOnly&useMessageIDAsCorrelationID=true")
+                .to("log:audiosSaida?showBody=false&showHeaders=true")
+                .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+                .setBody(header("TranscriptionID"));
 
         from("direct:okResponse")
-            .to("log:transacoes?showHeaders=true")
-            .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
-            .setBody(simple("OK"))
-        ;
+                .to("log:transacoes?showHeaders=true")
+                .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+                .setBody(simple("OK"));
     }
-    
 }
