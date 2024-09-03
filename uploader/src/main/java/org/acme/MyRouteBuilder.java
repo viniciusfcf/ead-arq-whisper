3783f99ee4fe673c5b0109ec9c8581e6b51dfe8b
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2024-09-02 22:12:32 -0300

    wip

--- uploader/src/main/java/org/acme/MyRouteBuilder.java
+++ uploader/src/main/java/org/acme/MyRouteBuilder.java
@@ -1,9 +1,11 @@
 package org.acme;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestParamType;
 
 public class MyRouteBuilder extends RouteBuilder {
 
@@ -11,12 +13,17 @@ public class MyRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
 
         rest("/hello")
-                .get("/say")
+                .get("")
+                .description("Endpoint que responde um simples OK")
                 .to("direct:okResponse");
 
         rest("/audios")
                 .post()
+                .description("Endpoint que faz transcrições de audios")
+                .type(List.class)
+                .param().name("body").type(RestParamType.body).endParam()
                 .consumes("application/octet-stream")
+                
                 .to("direct:sendMessage");
 
         from("direct:sendMessage")
